28c2be9b4ba710604acd609b858c211dcf59996d
EduGraminho <edugraminho@gmail.com>
2020-09-23 19:32:30 -0300

    adicionado teste snapshot, shallow enzyme no componente Index

--- src/components/input/__test__/index.test.jsx
+++ src/components/input/__test__/index.test.jsx
@@ -1,20 +1,58 @@
 import React from 'react'
 import Input from '../index'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 
-it('input renderizando corretamente', () => {
-  const tree = renderer
-    .create(
+describe('Teste Snapshot', () => {
+  it('input renderizando corretamente', () => {
+    const tree = renderer
+      .create(
+        <Input
+          id='id'
+          name='name'
+          onChange={() => {}}
+          placeholder='place'
+          value='value'
+          type='text'
+        />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('Teste Shallow Enzyme', () => {
+  it('simulando o onChange', () => {
+    const onChangeSpy = jest.fn()
+    const wrapper = shallow(<Input onChange={onChangeSpy} />)
+    wrapper.simulate('change')
+    expect(onChangeSpy).toHaveBeenCalled()
+  })
+
+  it('vendo se tem todos os argumentos', () => {
+    const wrapper = shallow(
       <Input
         id='id'
-        name='name'
-        onChange={() => {}}
-        placeholder='place'
         value='value'
-        type='text'
+        type='type'
+        placeholder='placeholder'
+        name='name'
+        onChange={[Function]}
       />
     )
-    .toJSON()
 
-  expect(tree).toMatchSnapshot()
+    expect(
+      wrapper.equals(
+        <input
+          id='id'
+          value='value'
+          type='type'
+          placeholder='placeholder'
+          name='name'
+          onChange={[Function]}
+        />
+      )
+    ).toBeTruthy()
+  })
 })
