9c5a1d0171f193d74ef76750611f8c331f33ef4b
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2020-09-24 14:57:47 -0300

    fixed css

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -13,6 +13,15 @@ const App = () => {
         alt='Homer Simpson'
         x={180}
       />
+      <Figure
+        src='https://440245-1378968-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/01/fatos-curiosos-sobre-o-homer-simpson.jpg'
+        alt='Homer Simpson'
+        y={260}
+      />
+      <Figure
+        src='https://440245-1378968-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/01/fatos-curiosos-sobre-o-homer-simpson.jpg'
+        alt='Homer Simpson'
+      />
     </>
   )
 }
--- src/components/figure/index.tsx
+++ src/components/figure/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { StyledFigure, Img, Figcaption } from './styles'
 
 interface FigProps {
@@ -8,15 +8,22 @@ interface FigProps {
   y?: number | string
 }
 
-const Figure = ({ src, alt, x, y }: FigProps) => {
+// const Figure = ({ src, alt, x, y }: FigProps) => {
+//   const maxWidth: string = x + 'px'
+//   const maxHeight: string = y + 'px'
+
+  const Figure = ({ src, alt, x = '100%', y = '100%' }: FigProps) => {
   const maxWidth: string = x ? x + 'px' : '100%'
   const maxHeight: number | string = y ? y + 'px' : '100%'
+    
   const style = { maxWidth, maxHeight }
   return (
-    <StyledFigure>
-      <Img src={src} alt={alt} style={style} />
-      <Figcaption>&ldquo;{alt}&rdquo;</Figcaption>
-    </StyledFigure>
+    <Div>
+      <StyledFigure>
+        <Img src={src} alt={alt} {{x || y} && style={style}} />
+        <Figcaption>&ldquo;{alt}&rdquo;</Figcaption>
+      </StyledFigure>
+    </Div>
   )
 }
 
